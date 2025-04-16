94c4804b2edb2a235a253685efdd955deff29e3f
august346 <ku113p@gmail.com>
2025-04-16 16:33:26 +0800

    upd - restruct

--- chapterts/01fundamentals/gotour/exercises/exercise05/interfaces.go
+++
@@ -1,9 +0,0 @@
-package exercise05
-
-import "fmt"
-
-type IPAddr [4]byte
-
-func (ip IPAddr) String() string {
-	return fmt.Sprintf("%v.%v.%v.%v", ip[0], ip[1], ip[2], ip[3])
-}
--- go.mod
+++
@@ -1,5 +0,0 @@
-module my-go-comeback
-
-go 1.24.2
-
-require golang.org/x/tour v0.1.0
