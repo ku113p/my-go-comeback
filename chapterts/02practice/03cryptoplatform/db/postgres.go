2a336e04465957f941ee3ad1ef3bc3bcc38795a4
august346 <ku113p@gmail.com>
2025-05-14 15:03:27 +0800

    upd|ref - use interface

--- chapterts/02practice/03cryptoplatform/db/postgres.go
+++ chapterts/02practice/03cryptoplatform/db/postgres.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"context"
 	"crypto/platform/models"
 	"fmt"
 
@@ -9,57 +10,51 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type PostgresDB struct {
+type RealDB struct {
 	idGenerator func() uuid.UUID
-	db          *sqlx.DB
+	db          sqlx.ExtContext
+	Close       func()
+	migrate     func() error
 }
 
-func (p *PostgresDB) newID() *uuid.UUID {
+func (p *RealDB) newID() *uuid.UUID {
 	id := p.idGenerator()
 	return &id
 }
 
-func (p *PostgresDB) Close() error {
-	if err := p.db.Close(); err != nil {
-		return err
-	}
-	return nil
+func (p *RealDB) Migrate() error {
+	return p.migrate()
 }
 
-func NewPostgresDBWithIDGen(dbURI string) (*PostgresDB, error) {
+func NewPostgresDBWithIDGen(dbURI string) (*RealDB, error) {
 	genID := func() uuid.UUID {
 		return uuid.New()
 	}
 
-	return newPostgresDB(genID, dbURI)
-}
-
-func newPostgresDB(idGenerator func() uuid.UUID, dbURI string) (*PostgresDB, error) {
 	db, err := sqlx.Connect("postgres", dbURI)
 	if err != nil {
 		return nil, err
 	}
-	return &PostgresDB{idGenerator, db}, nil
+	closeFunc := func() { db.Close() }
+	migrateFunc := func() error { return migratePostgreSQL(db.DB) }
+
+	return &RealDB{genID, db, closeFunc, migrateFunc}, nil
 }
 
-func (db *PostgresDB) Migrate() error {
-	return migratePostgreSQL(db.db.DB)
-}
-
-func (p *PostgresDB) UpdatePrices(prices []*models.TokenPrice) error {
-	_, err := p.db.NamedExec(`
-	INSERT INTO token_price (price, name, symbol, time)
-	VALUES (:price, :name, :symbol, :time)
-	ON CONFLICT (symbol) DO UPDATE
-	SET price = EXCLUDED.price,
-		name = EXCLUDED.name,
-		time = EXCLUDED.time;`, prices)
+func (p *RealDB) UpdatePrices(prices []*models.TokenPrice) error {
+	_, err := sqlx.NamedExecContext(context.TODO(), p.db, `
+		INSERT INTO token_price (price, name, symbol, time)
+		VALUES (:price, :name, :symbol, :time)
+		ON CONFLICT (symbol) DO UPDATE
+		SET price = EXCLUDED.price,
+			name = EXCLUDED.name,
+			time = EXCLUDED.time;`, prices)
 	return err
 }
 
-func (p *PostgresDB) GetPrice(symbol string) (*models.TokenPrice, error) {
+func (p *RealDB) GetPrice(symbol string) (*models.TokenPrice, error) {
 	prices := []*models.TokenPrice{}
-	err := p.db.Select(&prices, `SELECT * FROM token_price WHERE symbol = $1`, symbol)
+	err := sqlx.SelectContext(context.TODO(), p.db, &prices, `SELECT * FROM token_price WHERE symbol = $1`, symbol)
 	if err != nil {
 		return nil, err
 	}
@@ -72,18 +67,18 @@ func (p *PostgresDB) GetPrice(symbol string) (*models.TokenPrice, error) {
 	return nil, fmt.Errorf("too much prices")
 }
 
-func (p *PostgresDB) ListUsers() ([]*models.User, error) {
+func (p *RealDB) ListUsers() ([]*models.User, error) {
 	var users []*models.User
-	err := p.db.Select(&users, `SELECT * FROM users`)
+	err := sqlx.SelectContext(context.TODO(), p.db, &users, `SELECT * FROM users`)
 	if err != nil {
 		return nil, err
 	}
 	return users, nil
 }
 
-func (p *PostgresDB) GetUserByID(id uuid.UUID) (*models.User, error) {
+func (p *RealDB) GetUserByID(id uuid.UUID) (*models.User, error) {
 	users := []*models.User{}
-	err := p.db.Select(&users, `SELECT * FROM users WHERE id = $1`, id)
+	err := sqlx.SelectContext(context.TODO(), p.db, &users, `SELECT * FROM users WHERE id = $1`, id)
 	if err != nil {
 		return nil, err
 	}
@@ -96,9 +91,9 @@ func (p *PostgresDB) GetUserByID(id uuid.UUID) (*models.User, error) {
 	return nil, fmt.Errorf("too much users")
 }
 
-func (p *PostgresDB) GetUserByTelegramChatID(telegramChatID int64) (*models.User, error) {
+func (p *RealDB) GetUserByTelegramChatID(telegramChatID int64) (*models.User, error) {
 	users := []*models.User{}
-	err := p.db.Select(&users, `SELECT * FROM users WHERE telegram_chat_id = $1`, telegramChatID)
+	err := sqlx.SelectContext(context.TODO(), p.db, &users, `SELECT * FROM users WHERE telegram_chat_id = $1`, telegramChatID)
 	if err != nil {
 		return nil, err
 	}
@@ -111,37 +106,32 @@ func (p *PostgresDB) GetUserByTelegramChatID(telegramChatID int64) (*models.User
 	return nil, fmt.Errorf("too much users")
 }
 
-func (p *PostgresDB) CreateUser(user *models.User) (*models.User, error) {
+func (p *RealDB) CreateUser(user *models.User) (*models.User, error) {
 	user.ID = p.newID()
-
-	_, err := p.db.NamedExec(`INSERT INTO users (id, telegram_chat_id) VALUES (:id, :telegram_chat_id)`, user)
+	_, err := sqlx.NamedExecContext(context.TODO(), p.db, `INSERT INTO users (id, telegram_chat_id) VALUES (:id, :telegram_chat_id)`, user)
 	if err != nil {
 		return nil, err
 	}
-
 	return user, nil
 }
 
-func (p *PostgresDB) RemoveUser(id uuid.UUID) error {
-	_, err := p.db.Exec(`DELETE FROM users WHERE id = $1`, id)
-	if err != nil {
-		return err
-	}
-	return nil
+func (p *RealDB) RemoveUser(id uuid.UUID) error {
+	_, err := p.db.ExecContext(context.TODO(), `DELETE FROM users WHERE id = $1`, id)
+	return err
 }
 
-func (p *PostgresDB) ListNotificationsBySymbol(symbol string) ([]*models.Notification, error) {
+func (p *RealDB) ListNotificationsBySymbol(symbol string) ([]*models.Notification, error) {
 	var notifications []*models.Notification
-	err := p.db.Select(&notifications, `SELECT * FROM notification WHERE symbol = $1`, symbol)
+	err := sqlx.SelectContext(context.TODO(), p.db, &notifications, `SELECT * FROM notification WHERE symbol = $1`, symbol)
 	if err != nil {
 		return nil, err
 	}
 	return notifications, nil
 }
 
-func (p *PostgresDB) GetNotificationByID(id uuid.UUID) (*models.Notification, error) {
+func (p *RealDB) GetNotificationByID(id uuid.UUID) (*models.Notification, error) {
 	notifications := []*models.Notification{}
-	err := p.db.Select(&notifications, `SELECT * FROM notification WHERE id = $1`, id)
+	err := sqlx.SelectContext(context.TODO(), p.db, &notifications, `SELECT * FROM notification WHERE id = $1`, id)
 	if err != nil {
 		return nil, err
 	}
@@ -154,30 +144,25 @@ func (p *PostgresDB) GetNotificationByID(id uuid.UUID) (*models.Notification, er
 	return nil, fmt.Errorf("too much notifications")
 }
 
-func (p *PostgresDB) ListNotificationsByUserID(id uuid.UUID) ([]*models.Notification, error) {
+func (p *RealDB) ListNotificationsByUserID(id uuid.UUID) ([]*models.Notification, error) {
 	var notifications []*models.Notification
-	err := p.db.Select(&notifications, `SELECT * FROM notification WHERE user_id = $1`, id)
+	err := sqlx.SelectContext(context.TODO(), p.db, &notifications, `SELECT * FROM notification WHERE user_id = $1`, id)
 	if err != nil {
 		return nil, err
 	}
 	return notifications, nil
 }
 
-func (p *PostgresDB) CreateNotification(n *models.Notification) (*models.Notification, error) {
+func (p *RealDB) CreateNotification(n *models.Notification) (*models.Notification, error) {
 	n.ID = p.newID()
-
-	_, err := p.db.NamedExec(`INSERT INTO notification (id, user_id, symbol, sign, amount) VALUES (:id, :user_id, :symbol, :sign, :amount)`, n)
+	_, err := sqlx.NamedExecContext(context.TODO(), p.db, `INSERT INTO notification (id, user_id, symbol, sign, amount) VALUES (:id, :user_id, :symbol, :sign, :amount)`, n)
 	if err != nil {
 		return nil, err
 	}
-
 	return n, nil
 }
 
-func (p *PostgresDB) RemoveNotification(id uuid.UUID) error {
-	_, err := p.db.Exec(`DELETE FROM notification WHERE id = $1`, id)
-	if err != nil {
-		return err
-	}
-	return nil
+func (p *RealDB) RemoveNotification(id uuid.UUID) error {
+	_, err := p.db.ExecContext(context.TODO(), `DELETE FROM notification WHERE id = $1`, id)
+	return err
 }
